63d48ce02c3afa5ee6c3ae8565963ae2f6b3d5b7
karankrizz <kkriz448@gmail.com>
2026-07-30 22:55:05 +0530

    add:Task 2 kilometer to miles coversion completed

--- Day2.java
+++ Day2.java
@@ -3,14 +3,19 @@ public class Day2 {
 
         System.out.println("========================================");
         System.out.println("               DAY 2 TASKS              ");
-        System.out.println("         CELSIUS TO FAHRENHEIT          ");
         System.out.println("========================================");
 
+        System.out.println("\n[1] CELSIUS TO FAHRENHEIT");
         double celsius = 40.57;
         double fahrenheit = (celsius * 9 / 5) + 32;
-
         System.out.println("\nCelsius Value    : " + celsius + " °C");
         System.out.println("Fahrenheit Value : " + fahrenheit + " °F");
+
+        System.out.println("\n[2] KM TO MILES");
+        double kiloMeter = 15;
+        double miles = kiloMeter * 0.62137119;
+        System.out.println("\nKilometers : " + kiloMeter + " km");
+        System.out.println("Miles       : " + miles + " mi");
     }
-    
+
 }
