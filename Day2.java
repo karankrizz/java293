7e5c925365b8ed191e2b7e3089786d7de0a04e13
karankrizz <kkriz448@gmail.com>
2026-07-30 22:30:35 +0530

    Add Day2.java with Celsius to Fahrenheit conversion implementation task 1 completed

---
+++ Day2.java
@@ -0,0 +1,16 @@
+public class Day2 {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println("               DAY 2 TASKS              ");
+        System.out.println("         CELSIUS TO FAHRENHEIT          ");
+        System.out.println("========================================");
+
+        double celsius = 40.57;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("\nCelsius Value    : " + celsius + " °C");
+        System.out.println("Fahrenheit Value : " + fahrenheit + " °F");
+    }
+    
+}
