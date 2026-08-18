6f9a784fe46f90cebaa568b8d3d5e57e0219f710
karankrizz <kkriz448@gmail.com>
2026-08-18 23:49:48 +0530

    practice: add Day8 class demonstrating basic method usage with array output

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class Day8 {
+    public static void main(String args[]) {
+        System.out.println("\n[1] BASIC METHOD ");
+
+        int[] marks = { 90, 80, 70, 60, 50 };
+        System.out.print(marks[0] + " ");
+        System.out.print(marks[1] + " ");
+        System.out.print(marks[2] + " ");
+        System.out.print(marks[3] + " ");
+        System.out.print(marks[4] + " ");
+    }
+}
