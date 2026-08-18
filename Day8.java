2ee0b186552d0b5f3b51ad6814656b1553f04c32
karankrizz <kkriz448@gmail.com>
2026-08-18 23:50:27 +0530

    practice: enhance Day8 class with for loop example for array output

--- Day8.java
+++ Day8.java
@@ -8,5 +8,12 @@ public class Day8 {
         System.out.print(marks[2] + " ");
         System.out.print(marks[3] + " ");
         System.out.print(marks[4] + " ");
+
+        System.out.println("\n[2] USING THE FOR LOOP CONCEPT");
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+
     }
 }
