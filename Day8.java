3f9f36757c57a4aa31831b939793832495776750
karankrizz <kkriz448@gmail.com>
2026-08-18 23:51:45 +0530

    practice: enhance Day8 class with additional array sum calculation and output

--- Day8.java
+++ Day8.java
@@ -14,12 +14,22 @@ public class Day8 {
         for (int i = 0; i < marks.length; i++) {
             System.out.println(marks[i]);
         }
-        
+
         System.out.println("\n[3] USING STRING DATA TYPE WITH ARRAY USING THE FOR LOOP ");
 
         String[] names = { "kirubha", "karan", "R" };
         for (int i = 0; i < names.length; i++) {
             System.out.print(names[i] + " ");
         }
+
+        System.out.println("\n[4] SUM OF THE ARRAY VALUES");
+        int[] values = { 95, 98, 96, 97, 93 };
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+
+            sum = sum + values[i];
+            System.out.println("step :" + sum);
+        }
+        System.out.println("Total sum of the values : " + sum);
     }
 }
