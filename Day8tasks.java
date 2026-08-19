95e41d08d3c3548d69157091f317fce8a7c908c4
karankrizz <kkriz448@gmail.com>
2026-08-19 10:38:02 +0530

    task: find a specific array value and count even/odd numbers in Day8tasks class

--- Day8tasks.java
+++ Day8tasks.java
@@ -7,6 +7,33 @@ public class Day8tasks {
         for (int i = arr.length - 1; i >= 0; i--) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println(" ");
+        System.out.println("\n[2] FIND THE ARRAY VALUE ");
+        int[] numbers = { 0, 50, 60, 70, 2 };
+        int search = 2;
+        boolean found = false;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == search) {
+                found = true;
+                break;
+            }
+        }
+        System.out.println(found ? "Searched element: " + search + " is found" : "Element is not found");
+        System.out.println("\n[3] COUNT THE EVEN NUMBERS AND THE ODD NUMBERS ");
 
+        int[] values = { 20, 27, 57, 60, 97, 23, 88, 55, 100 };
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int i = 0; i < values.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+
+        }
+        System.out.println("Even numbers count: " + evenCount);
+        System.out.println("Odd numbers count: " + oddCount);
     }
 }
