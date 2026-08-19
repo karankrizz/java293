000d3350f0589a8fb929c9f752e61b22ec595c12
karankrizz <kkriz448@gmail.com>
2026-08-19 10:07:11 +0530

    Task:Day8tasks class to reverse and display an array

---
+++ Day8tasks.java
@@ -0,0 +1,12 @@
+public class Day8tasks {
+    public static void main(String args[]) {
+        System.out.println("\n[1] REVERSE THE ARRAY");
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
