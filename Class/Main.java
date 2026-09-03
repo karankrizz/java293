f38856b8168a3c968dce6d0463ab866146d1d052
karankrizz <kkriz448@gmail.com>
2026-09-03 14:26:48 +0530

    practice : Worked on the class,objects and method then using parameters in  methods.

---
+++ Class/Main.java
@@ -0,0 +1,79 @@
+// 1."CLASS WITH MULTIPLE OBJECTS"
+
+// class animal {
+//     String name;
+//     int age;
+//     String breed;
+
+// }
+
+// class Main {
+//     public static void main(String[] args) {
+//         animal a1 = new animal();
+
+//         a1.name = "tiger";
+//         a1.age = 5;
+//         a1.breed = "cat";
+//         System.out.println("Name of the animal :" + a1.name);
+//         System.out.println("Name of the breed :" + a1.breed);
+//         System.out.println("Animal age :" + a1.age);
+
+//         animal a2 = new animal();
+//         a2.name = "Lion";
+//         a2.age = 4;
+//         a2.breed = "cat";
+
+//         System.out.println("Name of the animal :" + a2.name);
+//         System.out.println("Name of the breed :" + a2.breed);
+//         System.out.println("Animal age :" + a2.age);
+
+//     }
+// }
+// CLASS WITH VARIABLE AND METHOD 
+
+// class Student {
+//     String name;
+//     int age;
+//     String course;
+
+//     void display() {
+//         System.out.println("Student Name : " + name);
+//         System.out.println("Student Age : " + age);
+//         System.out.println("Student Course : " + course);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Student s1 = new Student();
+//         s1.name = "Kirubhakaran R";
+//         s1.age = 25;
+//         s1.course = "Java Fullstack Development";
+//         s1.display();
+//         System.out.println(" --------------------");
+//         Student s2 = new Student();
+//         s2.name = "Arun";
+//         s2.age = 31;
+//         s2.course = "Python Fullstack Development";
+//         s2.display();
+//     }
+// }
+
+
+class Employee{
+    void display(String name, int age){
+        System.out.println("Employee Name : "+name);
+        System.out.println("Employee Age : "+ age);
+
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Employee e1=new Employee();
+        e1.display("Suresh", 30);
+        System.out.println("-----------");
+        Employee e2=new Employee();
+        e2.display("Ramesh", 28);
+
+    }
+}
